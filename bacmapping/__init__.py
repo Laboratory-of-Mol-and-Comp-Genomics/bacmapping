6ce76d69484372b6946457ded8a4d8840e98c590
Ewinden <ewinden@wisc.edu>
2023-01-10 16:51:56 -0600

    Add files via upload

---
+++ bacmapping/__init__.py
@@ -0,0 +1,6 @@
+from .bacmapping import (
+	getNewClones, narrowDownLibraries, splitAttributesWithMids,
+	getCuts, mapSequencedClones, mapPlacedClones, drawMap,
+	countPlacedBACs, splitAttributes, getCoverage, getAverageLength,
+	getSequencedClonesStats
+	)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bacmapping',
+      version='0.1',
+      description='A set of tools for mapping and understanding bacterial artificial chromosomes',
+      url='http://github.com/Ewinden/BACMapping',
+      author='Eamon Winden',
+      author_email='ewinden@wisc.edu',
+      license='',
+      packages=['bacmapping'],
+      zip_safe=False)
