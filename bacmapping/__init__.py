0bd18063b403bb635a2d5cf68ea580443fa85217
Ewinden <ewinden@wisc.edu>
2023-01-17 20:47:30 -0600

    Update __init__.py

--- bacmapping/__init__.py
+++ bacmapping/__init__.py
@@ -2,5 +2,6 @@ from .bacmapping import (
 	getNewClones, narrowDownLibraries, splitAttributesWithMids,
 	getCuts, mapSequencedClones, mapPlacedClones, drawMap,
 	countPlacedBACs, splitAttributes, getCoverage, getAverageLength,
-	getSequencedClonesStats
+	getSequencedClonesStats, onlySingleCutters, findPairs, makePairs,
+	getSequenceFromName, getSequenceFromLoc, getMapFromName, getMapsFromLoc
 	)
