06687288d59fa4dd05ecd794fc8fa0ceb8d7422f
Ewinden <ewinden@wisc.edu>
2023-02-28 09:39:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bacmapping',
       version='0.1',
       description='A set of tools for mapping and understanding bacterial artificial chromosomes',
-      url='http://github.com/Ewinden/BACMapping',
+      url='http://github.com/Ewinden/bacmapping',
       author='Eamon Winden',
       author_email='ewinden@wisc.edu',
       license='MIT',
